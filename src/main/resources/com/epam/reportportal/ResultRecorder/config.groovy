36518cdb094787006a0b53c9820ff81cc8c25261
iKozzz <minskcitizen@gmail.com>
2017-04-03 02:32:16 +0300

    update build

--- src/main/resources/com/epam/reportportal/ResultRecorder/config.groovy
+++ src/main/resources/com/epam/reportportal/ResultRecorder/config.groovy
@@ -4,10 +4,10 @@ f = namespace('lib/form')
 
 f.section(title: 'Parameters') {
     f.entry(title: "Rest endpoint") {
-        f.textbox(name: "endpoint", value: "https://rp.epam.com", field: 'endpoint')
+        f.textbox(name: "endpoint", field: 'endpoint')
     }
     f.entry(title: "Project") {
-        f.textbox(name: "project", value: "default_project", field: 'project')
+        f.textbox(name: "project", field: 'project')
     }
     f.entry(title: "Launch") {
         f.textbox(name: "launch", field: 'launch')
